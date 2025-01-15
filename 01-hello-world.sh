8e8b1ec5cdb4f7860d3be9d83917624c77453734
sundar <sundarsodadasi1996@gmail.com>
2025-01-15 20:09:09 +0530

    First statement

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Hi Team, I'm learning shell scripting"
+
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Hi Ramesh, Good Morning"
+echo "Ramesh: How Are You Suresh"
+echo "Suresh: I'm goog, how are you Ramesh"
