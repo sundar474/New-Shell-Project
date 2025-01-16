77b494ceb539e981485b5f5f5a9888452ee438f6
sundar <sundarsodadasi1996@gmail.com>
2025-01-16 09:44:22 +0530

    First Script

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-PERSON1=Sachin
-PERSON2=Rohit
-
-echo "Hi Team, I'm learning shell scripting"
-
-
-echo "$PERSON1: Hi $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, Good Morning"
-echo "$PERSON1: How Are You $PERSON2"
-echo "$PERSON2: I'm goog, how are you $PERSON1"
+echo "Hi Team, Today I'm giving demo on Shell Scripting"
