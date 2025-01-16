107cd547f2f2ad0b7511bca4e8b9895c517cc027
sundar <sundarsodadasi1996@gmail.com>
2025-01-16 10:24:12 +0530

    First Script

---
+++ 02-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Check if the number is greter than 10 or not
+NUMBER=$1
+
+if [ $NUMBER -gt 10]
+then
+     echo "$NUMBER is greter than 10"
+else
+     echo "$NUMBER is not greter than 10"
+fi
