c16c38940294274d3802c9e7914f61eccff67ca8
sundar <sundarsodadasi1996@gmail.com>
2025-01-16 10:38:20 +0530

    First Script

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# check if the number is greater than 10 or not
+#Check if the number is greater than 10 or not
 NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
@@ -8,4 +8,3 @@ then
     echo "$NUMBER is greater than 10"
 else
     echo "$NUMBER is not greater than 10"
-fi
