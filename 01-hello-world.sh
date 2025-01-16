c4de6fb9471d2e85b79a8f4d2278883ce69e077b
sundar <sundarsodadasi1996@gmail.com>
2025-01-16 09:48:59 +0530

    First Script

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Hi Team, Today I'm giving demo on Shell Scripting"
+
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Goood Morning Ramesh"
+echo "Ramesh: How Are You Suresh"
+echo "Suresh: I am good, How Are You"
