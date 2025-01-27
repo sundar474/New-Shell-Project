7a4bc12fe7187a3da483faea310007705e09652a
sundar <sundarsodadasi1996@gmail.com>
2025-01-27 13:25:06 +0530

    third commit

--- Install-mysql.sh
+++ Install-mysql.sh
@@ -14,3 +14,11 @@ fi
 
 # it is our responsibility again to check installation is success or not
 yum install mysqlll -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
