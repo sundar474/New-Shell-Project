d36104549d976ec8d670f65ab58fe570a45b5c9c
sundar <sundarsodadasi1996@gmail.com>
2025-01-27 15:46:28 +0530

    First commit

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello Team, I'm Learning Shell-Scripting"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How Are You Suresh"
+echo "Suresh: I am good, How are you Ramesh"
