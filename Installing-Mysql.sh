71fdcdf8d3f427bc31b0d2caa31d717fda1133b8
sundar <sundarsodadasi1996@gmail.com>
2025-01-27 16:21:49 +0530

    First commit

---
+++ Installing-Mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# our program goal is to install mysql
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+fi
+
+yum install mysql -y
