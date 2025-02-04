7623c01ac62550591fdb3d63f6557609f75d45b6
sundar <sundarsodadasi1996@gmail.com>
2025-02-04 14:41:06 +0530

    Adding EBS Volume

--- disk_usage.sh
+++ disk_usage.sh
@@ -16,9 +16,20 @@ Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filsystem')
 DISK_USAGE_THRESHOLD=1
-
+message=
 #IFS= means internal feild seperator is space.
 while IFS= read line
 do
-    echo "output: $line"
+    # this command will you usage in number format for comparisation
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    # this command will give us partition
+    partition=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    # now you need to check wether it is more than threshold or not 
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+    then
+        message+="HIGH DISK USAGE ON $partition: $usage"
+    fi
 done <<< $DISK_USAGE
+
+
+echo "message: $message"
