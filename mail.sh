aaa7e82d4f08a8606cc8874046bcd8c8eb0dc3f7
sundar <sundarsodadasi1996@gmail.com>
2025-02-04 23:58:04 +0530

    Adding EBS Volume

--- mail.sh
+++ mail.sh
@@ -13,4 +13,4 @@ echo "escap content: $BODY"
 
 FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
 
-echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
