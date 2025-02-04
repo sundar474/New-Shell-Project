76705a704163359ed09c60beee8b0a6c95700baa
sundar <sundarsodadasi1996@gmail.com>
2025-02-04 23:41:39 +0530

    Adding EBS Volume

---
+++ mail.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#anyone in your project can call this script with arguments
+#mail.sh TO_ADDRESS SUBJECT BODY TEAM_NAME ALERT_TYPE
+
+TO_ADDRESS=$1
+SUBJECT=$2
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+TEAM_NAME=$4
+ALERT_TYPE=$5
+echo "escap content: $BODY"
+#echo "all args: $@"
+
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY" -e template.html)
+
+echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
