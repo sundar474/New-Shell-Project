46f27eb06ff99a3a307dfe9c903f96447b40cb29
sundar <sundarsodadasi1996@gmail.com>
2025-02-04 16:29:45 +0530

    Adding EBS Volume

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# colours
-# Validations
+# colors
+# validations
 # log redirections
 
 LOGFILE_DIRECTORY=/tmp
@@ -14,22 +14,22 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filsystem')
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
-message=
+message=""
+
 #IFS= means internal feild seperator is space.
 while IFS= read line
-do
-    # this command will you usage in number format for comparisation
+do  
+    # this command will give you usage in number format for comparision
     usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
     # this command will give us partition
-    partition=$(echo $line | awk '{print $6}' | cut -d % -f1)
-    # now you need to check wether it is more than threshold or not 
+    partition=$(echo $line | awk '{print $1}')
+    #now you need to check whether it is more than threshold or not
     if [ $usage -gt $DISK_USAGE_THRESHOLD ];
     then
         message+="HIGH DISK USAGE on $partition: $usage"
     fi
 done <<< $DISK_USAGE
 
-
-echo "message: $message"
+echo -e "message: $message"
